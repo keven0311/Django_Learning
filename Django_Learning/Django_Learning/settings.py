7afa39565790ef28e2a309c58f0ba75477b12a36
kaichong lin <coco.keven@gmail.com>
2024-08-03 05:11:54 -0500

    fixed venv, installed python venv, Django media lession added.

--- Django_Learning/Django_Learning/settings.py
+++ Django_Learning/Django_Learning/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts',
-    'thetest'
+    'thetest',
 ]
 
 MIDDLEWARE = [
@@ -123,6 +123,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
