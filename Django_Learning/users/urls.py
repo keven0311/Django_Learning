92de7f0ee52d2822e8241c49d66f887999691156
kaichong lin <coco.keven@gmail.com>
2024-08-04 04:25:22 -0500

    user sign up function added

--- Django_Learning/users/urls.py
+++ Django_Learning/users/urls.py
@@ -5,5 +5,6 @@ app_name = 'users'
 
 urlpatterns = [
     path('', views.all_users, name='users_list'),
-    path('<str:name>', views.single_user, name='single_user'),
+    path('signup/',views.sign_up, name='sign_up'),
+    path('<str:name>/', views.single_user, name='single_user'),
 ]
--- Django_Learning/users/views.py
+++ Django_Learning/users/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Users
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def all_users(request):
@@ -12,4 +13,16 @@ def single_user(request,name):
     user = Users.objects.get(name=name)
     return render(request, 'users/single_user.html',{
         'single_user':user
+    })
+    
+def sign_up(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('users:users_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/sign_up.html',{
+        "form": form
     })
